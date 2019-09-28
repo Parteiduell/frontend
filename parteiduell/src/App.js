c0c229790e5b86092ab7bafc7b2f8c747e283bc8
jnnkB <29376715+jnnkB@users.noreply.github.com>
2019-09-28 19:05:54 +0200

    edited style

--- parteiduell/src/App.js
+++ parteiduell/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import "./App.css";
-import spd from "./pictures/spd.png";
 
 function importAll(r) {
   return r.keys().map(r);
@@ -11,142 +10,141 @@ const images = importAll(require.context("./pictures/", false, /\.(png|jpe?g|svg
 class Fragen extends Component {
 
   constructor(props) {
-  super(props);
-  this.state = {
-    isLoaded: false,
-    err: null,
-    items: [],
-    korrekt : null,
-    parties: []
-  };
-}
-componentDidMount() {
-  document.addEventListener("keydown", this.handleKeyDown.bind(this));
-  this.setState({
-    items: this.fetchItem()
-  });
-  console.log(images);
-}
-compare(partei){
-  return () => {
-    if(partei === this.state.items[0].answer){
-      this.setState({
-        korrekt: true
-      })
-    }else{
-      this.setState({
-        korrekt: false
-      })
-    }
+    super(props);
+    this.state = {
+      isLoaded: false,
+      err: null,
+      items: [],
+      korrekt: null,
+      parties: []
+    };
   }
-}
-getImage(partei) {
-  for (var image of images) {
-    if (image.includes(partei.toLowerCase().replace("/", "-"))){
-      return image;
-    }
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown.bind(this));
+    this.setState({
+      items: this.fetchItem()
+    });
+    console.log(images);
   }
-  console.error("Kein passendes Bild gefunden!", partei);
-}
-    handleKeyDown(e) {
-      if (e.key === "ArrowUp") {
-        this.compare(this.state.items[0].parteien[0])();
-      } else if (e.key === "ArrowLeft") {
-        this.compare(this.state.items[0].parteien[1])();
-      } else if (e.key === "ArrowDown") {
-        this.compare(this.state.items[0].parteien[2])();
-      } else if (e.key === "ArrowRight") {
-        this.compare(this.state.items[0].parteien[3])();
+  compare(partei) {
+    return () => {
+      if (partei === this.state.items[0].answer) {
+        this.setState({
+          korrekt: true
+        })
+      } else {
+        this.setState({
+          korrekt: false
+        })
       }
     }
-handleNext(){
-  this.setState({
-    korrekt: null,
-    items: this.fetchItem(),
-    isLoaded: false
-  })
-}
-
-fetchItem(){
-  fetch("https://solver.cloud:440/list") // fetch from REMOTE!
-  .then(result => result.json())
-    .then((res) => {
-      this.setState({
-        isLoaded: true,
-        items: res,
-      });
-      return res;
-    },
-    (error) => { //was passiert, wenn das ganze falsch läuft ???
-      this.setState({
-        isLoaded: false,
-        error
-      });
-      console.log(this.state);
-      console.log("AHHH");
+  }
+  getImage(partei) {
+    for (var image of images) {
+      if (image.includes(partei.toLowerCase().replace("/", "-"))) {
+        return image;
+      }
+    }
+    console.error("Kein passendes Bild gefunden!", partei);
+  }
+  handleKeyDown(e) {
+    if (e.key === "ArrowUp") {
+      this.compare(this.state.items[0].parteien[0])();
+    } else if (e.key === "ArrowLeft") {
+      this.compare(this.state.items[0].parteien[1])();
+    } else if (e.key === "ArrowDown") {
+      this.compare(this.state.items[0].parteien[2])();
+    } else if (e.key === "ArrowRight") {
+      this.compare(this.state.items[0].parteien[3])();
+    }
+  }
+  handleNext() {
+    this.setState({
+      korrekt: null,
+      items: this.fetchItem(),
+      isLoaded: false
     })
-}
-
-renderResult(){
-  const { items, korrekt } = this.state;
-  if(korrekt != null){
-    if(korrekt){
-      return(
-        <div>
-        <p>RICHTIG </p>
-        <button onClick={this.handleNext.bind(this)}> Nächste Frage </button>
-        </div>
-      )
-    }else{
-      return(
-        <div>
-      <p> Falsch, diese Aussage war von {items[0].answer} </p>
-      <button onClick={this.handleNext.bind(this)}> Nächste Frage </button>
-      </div>
-    )
   }
-}
-}
 
-  render(){
-    const { err, isLoaded, items } = this.state;
-    if(isLoaded){
-      var parties = Object.keys(items[0].possibleAnswers);
-      return(
-        <div>
-        <h2> ParteiDuell </h2>
-            {items.map(
-           item => (
-           <div>
-             <h2> {item.these} </h2>
-              <h3> {item.statement} </h3>
-              <div id="optionen">
-              {parties.map(
-                partei => (
-                  <div>
-                  <label class="parteien">
-                   <img src={this.getImage(partei)} alt={partei}/>
-                   <button onClick={this.compare(partei)}> {partei} </button>
-                  </label>
-                  </div>
-                )
-              )}
-              </div>
+  fetchItem() {
+    fetch("https://solver.cloud:440/list") // fetch from REMOTE!
+      .then(result => result.json())
+      .then((res) => {
+        this.setState({
+          isLoaded: true,
+          items: res,
+        });
+        return res;
+      },
+        (error) => { //was passiert, wenn das ganze falsch läuft ???
+          this.setState({
+            isLoaded: false,
+            error
+          });
+          console.log(this.state);
+          console.log("AHHH");
+        })
+  }
 
-
-            </div>
-          ))}
-            {this.renderResult()}
+  renderResult() {
+    const { items, korrekt } = this.state;
+    if (korrekt != null) {
+      if (korrekt) {
+        return (
+          <div>
+            <p>RICHTIG </p>
+            <button onClick={this.handleNext.bind(this)}> Nächste Frage </button>
           </div>
+        )
+      } else {
+        return (
+          <div>
+            <p> Falsch, diese Aussage war von {items[0].answer} </p>
+            <button onClick={this.handleNext.bind(this)}> Nächste Frage </button>
+          </div>
+        )
+      }
+    }
+  }
+
+  render() {
+    const { err, isLoaded, items } = this.state;
+    if (isLoaded) {
+      var parties = Object.keys(items[0].possibleAnswers);
+      return (
+        <div>
+          {items.map(
+            item => (
+              <div>
+                <h3 class="these"> {item.these} </h3>
+                <h2 class="statement"> {item.statement} </h2>
+                <div id="optionen">
+                  {parties.map(
+                    partei => (
+                      <div>
+                        <label class="parteien">
+                          <img src={this.getImage(partei)} alt={partei} />
+                          <button onClick={this.compare(partei)}> {partei} </button>
+                        </label>
+                      </div>
+                    )
+                  )}
+                </div>
+
+
+              </div>
+            ))}
+          {this.renderResult()}
+        </div>
       );
-  }else{
-    return(
-    <p> WAITING </p>
-  );
+    } else {
+      return (
+        <p> WAITING </p>
+      );
+    }
   }
+
+
 }
-
-
-  }
 
 export default Fragen;
