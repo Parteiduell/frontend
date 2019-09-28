1a29146f29e9c496aac4fc15fd6232f22a6e13d9
jnnkB <29376715+jnnkB@users.noreply.github.com>
2019-09-28 16:23:55 +0200

    Added keybindings

--- parteiduell/src/App.js
+++ parteiduell/src/App.js
@@ -3,86 +3,102 @@ import React, { Component } from 'react';
 class Fragen extends Component {
 
   constructor(props) {
-  super(props);
-  this.state = {
-    isLoaded: false,
-    err: null,
-    items: [],
-    korrekt : null,
+    super(props);
+    this.state = {
+      isLoaded: false,
+      err: null,
+      items: [],
+      korrekt: null,
 
-  };
-}
-componentDidMount() {
-  this.setState({
-    isLoaded: true,
-    items: [{thema: "Umwelt", frage: "Sollte Deutschland mehr CO2 einsparen?", parteien: ["CDU", "SPD", "AfD", "LINKE"], antwort: "LINKE", kontext: null }]
-  });
+    };
+  }
+  componentWillMount() {
+    document.addEventListener("keydown", this.handleKeyDown.bind(this))
+  }
+  componentDidMount() {
+    this.setState({
+      isLoaded: true,
+      items: [{ thema: "Umwelt", frage: "Sollte Deutschland mehr CO2 einsparen?", parteien: ["CDU", "SPD", "AfD", "LINKE"], antwort: "LINKE", kontext: null }]
+    });
 
-  // fetch("https://pa-clubs.herokuapp.com/database"). // fetch from REMOTE!
-  // then(result => result.json())
-  //   .then((res) => {
-  //     console.log(res); //wahrscheinlich nicht
-  //     this.setState({
-  //       isLoaded: true,
-  //       items: res
-  //     });
-  //   },
-  //   (error) => { //was passiert, wenn das ganze falsch läuft ???
-  //     this.setState({
-  //       isLoaded: false,
-  //       error
-  //     });
-  //     console.log(this.state)
-  //   })
-}
-compare(partei){
-  return () => {
-    if(partei === this.state.items[0].antwort){
-      this.setState({
-        korrekt: true
-      })
-    }else{
-      this.setState({
-        korrekt: false
-      })
+    // fetch("https://pa-clubs.herokuapp.com/database"). // fetch from REMOTE!
+    // then(result => result.json())
+    //   .then((res) => {
+    //     console.log(res); //wahrscheinlich nicht
+    //     this.setState({
+    //       isLoaded: true,
+    //       items: res
+    //     });
+    //   },
+    //   (error) => { //was passiert, wenn das ganze falsch läuft ???
+    //     this.setState({
+    //       isLoaded: false,
+    //       error
+    //     });
+    //     console.log(this.state)
+    //   })
+  }
+  compare(partei) {
+    return () => {
+      if (partei === this.state.items[0].antwort) {
+        this.setState({
+          korrekt: true
+        })
+      } else {
+        this.setState({
+          korrekt: false
+        })
+      }
+    };
+  }
+
+  handleKeyDown(e) {
+    if (e.key === "ArrowUp") {
+      this.compare(this.state.items[0].parteien[0])();
+    } else if (e.key === "ArrowLeft") {
+      this.compare(this.state.items[0].parteien[1])();
+    } else if (e.key === "ArrowDown") {
+      this.compare(this.state.items[0].parteien[2])();
+    } else if (e.key === "ArrowRight") {
+      this.compare(this.state.items[0].parteien[3])();
     }
-  };
-}
 
-renderResult(){
-  const {items, korrekt } = this.state;
-  if(korrekt != null){
-    if(korrekt){
-      return(
-        <p> HELLO WORLD </p>
-      )
-    }else{
-      return(
-      <p> BYE WORLD </p>
-    )
+  }
+
+  renderResult() {
+    const { items, korrekt } = this.state;
+    if (korrekt != null) {
+      if (korrekt) {
+        return (
+          <p> HELLO WORLD </p>
+        )
+      } else {
+        return (
+          <p> BYE WORLD </p>
+        )
+      }
     }
   }
-}
   render() {
     const { err, isLoaded, items } = this.state;
-    console.log(items);
+
     return (
       <div>
-      <h2> ParteiDuell </h2>
-          {items.map(
-         item =>
-         <div>
-           <h2> {item.thema} </h2>
-            <h3> {item.frage} </h3>
-            {item.parteien.map(
-              partei =>
-                <button onClick={this.compare(partei)}> {partei} </button>
-            )}
-          </div>
+        <h2> ParteiDuell </h2>
+        {items.map(
+          item =>
+            <div>
+              <h2> {item.thema} </h2>
+              <h3> {item.frage} </h3>
+              {item.parteien.map(
+                partei =>
+                  <button onClick={this.compare(partei)}> {partei} </button>
+              )}
+            </div>
         )}
-          {this.renderResult()}
+        {this.renderResult()}
 
-        </div>
+      </div>
     );
   }
 }
