ed184339fbfcdc6d18c43ade3749808e80a7d992
Gruselhaus <nico@gruselhaus.com>
2019-10-05 12:00:44 +0200

    Cookies Reset

--- src/Settings.js
+++ src/Settings.js
@@ -1,6 +1,6 @@
-import Select from 'react-select';
+import Select from "react-select";
 import React, { Component } from "react";
-import Cookies from 'universal-cookie';
+import Cookies from "universal-cookie";
 
 const cookies = new Cookies();
 
@@ -9,131 +9,134 @@ const sources = [];
 var fetched = false;
 
 function sortAlphabetically(a, b) {
-    a = a.toLowerCase();
-    b = b.toLowerCase();
+  a = a.toLowerCase();
+  b = b.toLowerCase();
 
-    if (a < b) {
-        return -1
-    }
-    if (a > b) {
-        return 1
-    }
-    return 0
+  if (a < b) {
+    return -1;
+  }
+  if (a > b) {
+    return 1;
+  }
+  return 0;
 }
 class Settings extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        const selectedParties = cookies.get("selectedParties")
-        const selectedSources = cookies.get("selectedSources")
+    const selectedParties = cookies.get("selectedParties");
+    const selectedSources = cookies.get("selectedSources");
 
-        this.state = {
-            closed: true,
-            selectedParties: selectedParties ? selectedParties : [
-                "SPD",
-                "CDU/CSU",
-                "GRÜNE",
-                "FDP",
-                "PIRATEN",
-                "DIE LINKE",
-                "NPD",
-                "Die PARTEI",
-                "AfD"
-            ],
-            selectedSources: selectedSources ? selectedSources : [
-                "Bundestagswahl 2005",
-                "Bundestagswahl 2009",
-                "Bundestagswahl 2013",
-                "Bundestagswahl 2017"
-            ]
-        };
+    this.state = {
+      closed: true,
+      selectedParties: selectedParties
+        ? selectedParties
+        : ["SPD", "CDU/CSU", "GRÜNE", "FDP", "PIRATEN", "DIE LINKE", "NPD", "Die PARTEI", "AfD"],
+      selectedSources: selectedSources
+        ? selectedSources
+        : ["Bundestagswahl 2005", "Bundestagswahl 2009", "Bundestagswahl 2013", "Bundestagswahl 2017"]
+    };
+  }
+
+  handlePartiesChange(selectedParties) {
+    if (selectedParties.length >= 2) {
+      this.setState({ selectedParties: selectedParties.map(party => party.value) });
     }
+  }
 
-    handlePartiesChange(selectedParties) {
-      if (selectedParties.length >= 2) {
-        this.setState(({ "selectedParties": selectedParties.map(party => party.value) }));
-      }
+  handleSourcesChange(selectedSources) {
+    this.setState({ selectedSources: selectedSources.map(source => source.value) });
+  }
+
+  close() {
+    this.setState({ closed: true });
+    this.props.onClose();
+    cookies.set("selectedSources", this.state.selectedSources, { path: "/", maxAge: 60 * 60 * 24 * 3 });
+    cookies.set("selectedParties", this.state.selectedParties, { path: "/", maxAge: 60 * 60 * 24 * 3 });
+  }
+
+  reset() {
+    if (document.cookie.indexOf("selectedSources") >= 0) cookies.remove("selectedSources");
+    if (document.cookie.indexOf("selectedParties") >= 0) cookies.remove("selectedParties");
+    this.setState({ closed: true });
+    this.props.onClose();
+  }
+
+  show() {
+    this.setState({ closed: false });
+    if (!fetched) {
+      fetch(window.url.replace("/list", "/allParties"))
+        .then(result => result.json())
+        .then(result => result.sort(sortAlphabetically))
+        .then(result => {
+          result.forEach(party => {
+            parties.push({ value: party, label: party });
+          });
+        });
+
+      fetch(window.url.replace("/list", "/allSources"))
+        .then(result => result.json())
+        .then(result => result.sort(sortAlphabetically))
+        .then(result => {
+          result.forEach(source => {
+            sources.push({ value: source, label: source });
+          });
+        });
+      fetched = true;
     }
+  }
 
-    handleSourcesChange(selectedSources) {
-        this.setState(({ "selectedSources": selectedSources.map(source => source.value) }));
+  getSettings() {
+    return this.state;
+  }
+
+  render() {
+    const selectedParties = this.state.selectedParties.map(party => {
+      return { label: party, value: party };
+    });
+    const selectedSources = this.state.selectedSources.map(source => {
+      return { label: source, value: source };
+    });
+
+    if (!this.state.closed) {
+      return (
+        <div className="overlay">
+          <div className="settings">
+            <h2>Parteien</h2>
+            <Select
+              className="select"
+              classNamePrefix="select"
+              value={selectedParties}
+              onChange={this.handlePartiesChange.bind(this)}
+              options={parties}
+              isMulti={true}
+              closeMenuOnSelect={false}
+            />
+            <h2>Quellen</h2>
+            <Select
+              className="select"
+              classNamePrefix="select"
+              value={selectedSources}
+              onChange={this.handleSourcesChange.bind(this)}
+              options={sources}
+              isMulti={true}
+              closeMenuOnSelect={false}
+            />
+            <label className="next">
+              <button onClick={this.reset.bind(this)}></button>
+              Reset
+            </label>
+            <label className="next">
+              <button onClick={this.close.bind(this)}></button>
+              Schließen
+            </label>
+          </div>
+        </div>
+      );
+    } else {
+      return null;
     }
-
-    close() {
-        this.setState({ closed: true })
-        this.props.onClose();
-        cookies.set("selectedSources", this.state.selectedSources, { path: '/', maxAge: 60 * 60 * 24 * 3 });
-        cookies.set("selectedParties", this.state.selectedParties, { path: '/', maxAge: 60 * 60 * 24 * 3 });
-    }
-
-    show() {
-        this.setState({ closed: false })
-        if (!fetched) {
-            fetch(window.url.replace("/list", "/allParties"))
-                .then(result => result.json())
-                .then(result => result.sort(sortAlphabetically))
-                .then(result => {
-                    result.forEach(party => {
-                        parties.push({ value: party, label: party })
-                    });
-                });
-
-            fetch(window.url.replace("/list", "/allSources"))
-                .then(result => result.json())
-                .then(result => result.sort(sortAlphabetically))
-                .then(result => {
-                    result.forEach(source => {
-                        sources.push({ value: source, label: source })
-                    });
-                });
-            fetched = true;
-        }
-    }
-
-    getSettings() {
-        return this.state;
-    }
-
-    render() {
-        const selectedParties = this.state.selectedParties.map(party => { return { label: party, value: party } });
-        const selectedSources = this.state.selectedSources.map(source => { return { label: source, value: source } });
-
-        if (!this.state.closed) {
-            return (
-                <div className="overlay">
-                    <div className="settings">
-                        <h2>Parteien</h2>
-                        <Select
-                            className="select"
-                            classNamePrefix="select"
-                            value={selectedParties}
-                            onChange={this.handlePartiesChange.bind(this)}
-                            options={parties}
-                            isMulti={true}
-                            closeMenuOnSelect={false}
-                        />
-                        <h2>Quellen</h2>
-                        <Select
-                            className="select"
-                            classNamePrefix="select"
-                            value={selectedSources}
-                            onChange={this.handleSourcesChange.bind(this)}
-                            options={sources}
-                            isMulti={true}
-                            closeMenuOnSelect={false}
-                        />
-                        <label className="next">
-                            <button onClick={this.close.bind(this)}></button>
-                            Schließen
-                        </label>
-                    </div >
-
-                </div>
-            );
-        } else {
-            return null;
-        }
-    }
+  }
 }
 
 export default Settings;
